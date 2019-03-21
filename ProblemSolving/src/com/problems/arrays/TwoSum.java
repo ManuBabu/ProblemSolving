5a4174de81ad2a6f548abeb0b7d3e70e3db9cd50
manimkv <manikandanv3131@gmail.com>
2019-03-21 23:00:22 +0530

    #DynamicPrograming - LongestSubSequence.

--- ProblemSolving/src/com/problems/arrays/TwoSum.java
+++ ProblemSolving/src/com/problems/arrays/TwoSum.java
@@ -1,5 +1,7 @@
 package com.problems.arrays;
 
+import java.util.HashMap;
+
 public class TwoSum {
 
 
@@ -20,7 +22,31 @@ public class TwoSum {
 
 	
 	public static void main(String[] args) {
+	
+		// My Solution
+	
+		int[] nums = {3,2,4};
+		int target =6;
 		
-	}
+		   HashMap<Integer,Integer> map =  new HashMap();
 
+	        int[] output = new int[2];
+	        
+	        int i=0;
+	       for(int num :nums)
+	           map.put(num,i++);
+
+	        for(int a: nums)
+	            if(map.containsKey(Math.abs(target-a))){
+	                
+	                output[0]=map.get(a);
+	                if(map.get(Math.abs(target-a))!=output[0]) {
+	                output[1]=map.get(Math.abs(target-a));
+	                break;
+	                }
+	            }
+	       
+	        System.out.println(output[0]+" : "+output[1]);
+		
+		}
 }
